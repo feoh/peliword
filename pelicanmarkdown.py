91fd3dac136f519056960f7ccc448593b0dfe06b
Chris Patti <cpatti@gmail.com>
2017-01-30 02:39:49 +0000

    Make converter cycle through posts dir.

--- pelicanmarkdown.py
+++ pelicanmarkdown.py
@@ -1,12 +1,20 @@
+import glob
+import os
 import markdown
 import pelican
 import requests
 import pprint
-import wp_client_secret
+from peliword_config import pelican_blog_dir, wp_client_secret
 
 settings = pelican.settings.read_settings()
 mdr = pelican.readers.MarkdownReader(settings)
-mda = mdr.read("/home/feoh/src/personal/blindnotdumb/content/Bacon+Microwave=Jerky.md")
-headers = mda[1]
-print("title: {}".format(headers['title']))
+
+if not os.path.exists(pelican_blog_dir):
+    print("Not a valid Pelican blog directory: {}".format(pelican_blog_dir))
+
+glob_path = pelican_blog_dir + "/content/*.md"
+for post in glob.glob(glob_path):
+    mda = mdr.read(post)
+    headers = mda[1]
+    print("title: {}".format(headers['title']))
 
