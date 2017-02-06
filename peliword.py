ec25cf5303afe26dc7e806266654d794b9f527c2
Chris Patti <cpatti@gmail.com>
2017-02-06 02:37:08 +0000

    Add parsing the tags

--- peliword.py
+++ peliword.py
@@ -7,6 +7,7 @@ import requests
 import pprint
 import json
 import html
+from pelican.utils import SafeDatetime
 
 from peliword_config import pelican_blog_dir, wp_client_secret
 
@@ -15,17 +16,13 @@ wp_api_base = 'https://public-api.wordpress.com/rest/v1.1/sites/feohorg.wordpres
 settings = pelican.settings.read_settings()
 mdr = pelican.readers.MarkdownReader(settings)
 
-wp_posts_response = requests.get(wp_api_base + "/posts" )
-wp_posts_json = wp_posts_response.text
-wp_resp = json.loads(wp_posts_json)
+wp_get_posts_response = requests.get(wp_api_base + "/posts" )
+wp_get_posts_json = wp_get_posts_response.text
+wp_get_resp = json.loads(wp_get_posts_json)
 
-wp_posts = wp_resp['posts']
+wp_get_posts = wp_get_resp['posts']
 
-wp_titles = [ html.unescape(wp_post['title']) for wp_post in wp_posts ]
-
-print(wp_titles)
-# pprint.pprint(wp_posts)
-
+wp_titles = [ html.unescape(wp_post['title']) for wp_post in wp_get_posts ]
 
 if not os.path.exists(pelican_blog_dir):
     print("Not a valid Pelican blog directory: {}".format(pelican_blog_dir))
@@ -33,7 +30,30 @@ if not os.path.exists(pelican_blog_dir):
 glob_path = pelican_blog_dir + "/content/*.md"
 for post in glob.glob(glob_path):
     mda = mdr.read(post)
-    headers = mda[1]
-    title = headers['title']
-    if title not in wp_titles:
-        print("title: {}".format(title))
+    pelican_headers = mda[1]
+    pprint.pprint(pelican_headers)    
+    pelican_tag_list = pelican_headers['tags']
+    pelican_tags = [ tag.name for tag in pelican_tag_list ].
+    print("tags: {}".format(pelican_tags))
+
+    next
+    pelican_body = mda[0]
+    if pelican_headers['title'] not in wp_titles:
+        print("Posting title: {}".format(pelican_headers['title']))
+        headers = { 'authorization', 'Bearer {}'.format(wp_client_secret)}
+
+        test_post = "Date: {}\nTitle: {}\nAuthor: {}\ntags: {}\ncategory: {}\n".format(pelican_headers['date'],
+                                                                                       pelican_headers['title'],
+                                                                                       pelican_headers['author'],
+                                                                                       pelican_headers['tags'],
+                                                                                       pelican_headers['category'])
+        print(test_post)
+
+        
+        # wp_post_response = requests.post(wp_api_base + "/posts/new", data = {'title': pelican_headers['title'],
+        #                                                                      'author': pelican_headers['author'],
+        #                                                                      'tags': pelican_headers['tags'],
+        #                                                                      'date': pelican_headers['date'],
+        #                                                                      'category': pelican_headers['category'],
+        #                                                                      'content' : pelican_body})
+        #pprint.pprint(wp_post_response)
