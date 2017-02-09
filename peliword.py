97f6fdd1b47cbae020a10d09137c8be7edaa0280
Chris Patti <cpatti@gmail.com>
2017-02-09 03:34:39 +0000

    Struggling towards a working http post :)

--- peliword.py
+++ peliword.py
@@ -7,9 +7,22 @@ import requests
 import pprint
 import json
 import html
+import logging
 from pelican.utils import SafeDatetime
-
 from peliword_config import pelican_blog_dir, wp_client_secret
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
+http_client.HTTPConnection.debuglevel = 1
+
+# You must initialize logging, otherwise you'll not see debug output.
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
 
 wp_api_base = 'https://public-api.wordpress.com/rest/v1.1/sites/feohorg.wordpress.com'
 
@@ -31,29 +44,41 @@ glob_path = pelican_blog_dir + "/content/*.md"
 for post in glob.glob(glob_path):
     mda = mdr.read(post)
     pelican_headers = mda[1]
-    pprint.pprint(pelican_headers)    
-    pelican_tag_list = pelican_headers['tags']
-    pelican_tags = [ tag.name for tag in pelican_tag_list ].
-    print("tags: {}".format(pelican_tags))
+    pprint.pprint(pelican_headers)
+    # Pelican tags are actually objects. Process them down to a comma separated string of tags
+    pelican_tag_objs = pelican_headers['tags']
+    pelican_tag_list = [ tag.name for tag in pelican_tag_objs ]
+    pelican_tags = ",".join(pelican_tag_list)
 
-    next
+    wp_post_headers = pelican_headers
+    wp_post_headers['tags'] = pelican_tags
+
+    # Do the same for author.
+    author = wp_post_headers['author'].name
+    wp_post_headers['author'] = author
+    
     pelican_body = mda[0]
     if pelican_headers['title'] not in wp_titles:
         print("Posting title: {}".format(pelican_headers['title']))
-        headers = { 'authorization', 'Bearer {}'.format(wp_client_secret)}
+        print("wp_client_secret={}".format(wp_client_secret))
+        bearer = "Bearer {}".format(wp_client_secret)
+        pprint.pprint(bearer)
+        headers = {}
+        headers['authorization'] = bearer
 
-        test_post = "Date: {}\nTitle: {}\nAuthor: {}\ntags: {}\ncategory: {}\n".format(pelican_headers['date'],
-                                                                                       pelican_headers['title'],
-                                                                                       pelican_headers['author'],
-                                                                                       pelican_headers['tags'],
-                                                                                       pelican_headers['category'])
+        print("headers:")
+        pprint.pprint(headers)
+
+        test_post = "Date: {}\nTitle: {}\nAuthor: {}\ntags: {}\ncategory: {}\n".format(wp_post_headers['date'],
+                                                                                       wp_post_headers['title'],
+                                                                                       wp_post_headers['author'],
+                                                                                       wp_post_headers['tags'],
+                                                                                       wp_post_headers['category'])
         print(test_post)
 
         
-        # wp_post_response = requests.post(wp_api_base + "/posts/new", data = {'title': pelican_headers['title'],
-        #                                                                      'author': pelican_headers['author'],
-        #                                                                      'tags': pelican_headers['tags'],
-        #                                                                      'date': pelican_headers['date'],
-        #                                                                      'category': pelican_headers['category'],
-        #                                                                      'content' : pelican_body})
-        #pprint.pprint(wp_post_response)
+        wp_post_response = requests.post(wp_api_base + "/posts/new", headers = headers, data = pelican_headers)
+        pprint.pprint(wp_post_response)
+        break;
+
+
